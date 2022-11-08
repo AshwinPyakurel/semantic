5feb61de17a7d6afb12d993ed7fdaf1d1a1f82d9
ashwinpyakurel <ashwinpyakurel57@gmail.com>
2022-11-08 14:48:47 +0545

    feat: Hello

--- semantic/index.ts
+++ semantic/index.ts
@@ -1,1 +1,1 @@
-console.log("hello world to happy hhhhhhhhhhhh hh  hh  hhh kjsdfa=fakkjsdbf----");
+console.log("hello world to happy hhhhhhhhhhhh hh  hh  hhh kjsdfa=fakkjsdbf---  adflk-");
--- test-first/index.js
+++ test-first/index.js
@@ -1,1 +1,1 @@
-console.log("HELLOOOOOOOOO kjasdhflk ausdhfakjgsd ajgfakjhdsgasdfha aksjdhal");
+console.log("HELLOOOOOOOOO");
