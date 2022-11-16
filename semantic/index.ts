9306c3f1d9fad54b8f8fe2eaafb343eb47479021
ashwinpyakurel <ashwinpyakurel57@gmail.com>
2022-11-16 09:34:45 +0545

    chore: check check

--- semantic/index.ts
+++ semantic/index.ts
@@ -1,1 +1,1 @@
-console.log("hello world to happy hhhhhhhhhhhh hh  hh  hhh kjsdfa= non akjdfajkdsfhakjaksjdfh--  akjsdhfadflk-");
+console.log("hello world to happy hhhhhhhhhhhh hh  hh  hhh kjsdfa= non akjdfajkdsfhakjaksjajhdfadflk-");
